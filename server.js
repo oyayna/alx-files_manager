6fed8a6fe1ed1753f4d99de7f8bb9912c4bd9dc0
Kamado Tanjiro <melio@kali>
2024-08-09 19:22:18 -0400

    Update

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
+
